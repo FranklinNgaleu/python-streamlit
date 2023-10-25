2af6bc026921920ba4b7449b550c7e00170183bb
FranklinNgaleu <franklin.tchokodjeu@institutsaintjean.org>
2023-10-25 17:02:29 +0200

    Update app3.py

--- pages/app3.py
+++ pages/app3.py
@@ -5,5 +5,17 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
+    
+    # Afficher les statistiques des données
+    st.write("Statistics for the CSV file:")
+    st.write(bytes_data.describe())
+    
+    # Créer un widget de sélection des colonnes
+    col = st.multiselect("colonne ", bytes_data.col)
+
+    if col:
+        # Afficher les statistiques des colonnes sélectionnées
+        st.write("statistique de la colonne:")
+        st.write(bytes_data[col].describe())
 
 st.sidebar.title("Menu")
